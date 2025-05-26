2564e6aab2bcb7b9e92df5036de106beacab7b5d
Omar Kraidié <omppula_1@hotmail.com>
2025-05-26 07:31:09 +0300

    Updated form error messages.

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,6 +1,24 @@
 import { z } from 'zod';
 
 export const shoppingListformSchema = z.object({
-	name: z.string().min(2).max(50),
-	amount: z.coerce.number().min(1).max(1000),
+	name: z
+		.string()
+		.min(2, {
+			message: 'Name must be at least 2 characters long',
+		})
+		.max(50, {
+			message: 'Name must not exceed 50 characters',
+		})
+		.max(50, {
+			message: 'Name must not exceed 50 characters',
+		}),
+	amount: z.coerce
+		.number()
+		.min(1, {
+			message: 'Amount must be at least 1',
+		})
+		.max(1000, {
+			message: 'Amount must not exceed 1000',
+		})
+		.max(1000),
 });
